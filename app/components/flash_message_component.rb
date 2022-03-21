23a0e76c3fd4c0f5d527098b9a6261687fd3d7d6
Francisco Sousa <francisco.sousa@hey.com>
2022-03-21 00:32:32 +0000

    Fix tailwind class

--- app/components/flash_message_component.rb
+++ app/components/flash_message_component.rb
@@ -3,23 +3,9 @@ class FlashMessageComponent < ViewComponent::Base
     super
     @message = message
     @type = type
-    @alert_class = alert_class_name(type)
   end
 
   def render?
     @message.present?
-  end
-
-  private
-
-  def alert_class_name(type)
-    case type
-    when :alert
-      "alert-warning"
-    when :notice
-      "alert-info"
-    else
-      ""
-    end
   end
 end
